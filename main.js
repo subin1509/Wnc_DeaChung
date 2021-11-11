0feb96430150e7df849095a985a836bb7b957b1f
donghun <zhfptm12@naver.com>
2021-11-11 17:07:47 +0900

    mainWnc

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("login_btn").addEventListener("click", click);
+document
+    .getElementById("teacherList")
+    .addEventListener("click", click_teacherList);
+function click_teacherList() {
+    open("./teacherList.html");
+}
+
+function click() {
+    open("./sample.html");
+}
