4987c642d420b9b2e25a723fc7256ffc3891245e
subin1509 <subin1509@naver.com>
2021-11-11 18:52:00 +0900

    js

--- managerPage.js
+++ managerPage.js
@@ -2,4 +2,8 @@ let searchBtn = document.getElementById("searchBtn");
 let blacklistBtn = document.getElementById("blacklistBtn");
 let searchSelect = document.getElementById("searchSelect");
 
-console.log(searchSelect.options[searchSelect.selectedIndex].text);
+searchSelect.addEventListener("change", changeSearch);
+
+function changeSearch(){
+    console.log(searchSelect.options[searchSelect.selectedIndex].text);
+}
