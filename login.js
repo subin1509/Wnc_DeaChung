f9e6b72f55590b8cab66be1c2ab233d0c654b79f
uu2727318 <81275714+uu2727318@users.noreply.github.com>
2021-11-11 16:41:54 +0900

    login

--- login.js
+++ login.js
@@ -4,6 +4,7 @@ sign_up.addEventListener("click", signup_display);
 var cancel = document.getElementById("cancel");
 cancel.addEventListener("click", cancel_display);
 
+
 document.getElementById("signup_box").style.display="none";
 function signup_display() {
     document.getElementById("signup_box").style.display="block";
