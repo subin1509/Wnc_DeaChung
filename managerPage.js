5dc3415ac5525801550d0c6c137de98a3822caf1
subin1509 <subin1509@naver.com>
2021-11-11 18:47:02 +0900

    js commit test

---
+++ managerPage.js
@@ -0,0 +1,5 @@
+let searchBtn = document.getElementById("searchBtn");
+let blacklistBtn = document.getElementById("blacklistBtn");
+let searchSelect = document.getElementById("searchSelect");
+
+console.log(searchSelect.options[searchSelect.selectedIndex].text);
