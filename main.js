01755222565f341c46ac1ef84d657b48e80e5723
donghun <zhfptm12@naver.com>
2021-11-13 19:00:57 +0900

    main

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ document.getElementById("login_btn").addEventListener("click", click);
 document
     .getElementById("teacherList")
     .addEventListener("click", click_teacherList);
+function init() {
+    let classfication = localStorage.getItem("classfication");
+    let id = localStorage.getItem("id");
+    let name = localStorage.getItem("name");
+
+    if (classfication == "student") {
+    } else if (classfication == "teacher") {
+        document.getElementById("my_board").style.display = "inline-block";
+        document.getElementById("register_board").style.display =
+            "inline-block";
+    } else {
+    }
+}
 function click_teacherList() {
     open("./teacherList.html");
 }
@@ -9,3 +22,4 @@ function click_teacherList() {
 function click() {
     open("./login.html");
 }
+init();
--- main_db.js
+++ main_db.js
@@ -1,19 +1,33 @@
-db.collection("users")
-    .add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815,
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-    });
-db.collection("users")
-    .get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-        });
+// db.collection("users")
+//     .get()
+//     .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             console.log(`${doc.id} => ${doc.data()}`);
+//         });
+//     });
+function format(date) {
+    var year = date.getFullYear(); //yyyy
+    var month = 1 + date.getMonth(); //M
+    month = month >= 10 ? month : "0" + month; //month 두자리로 저장
+    var day = date.getDate(); //d
+    day = day >= 10 ? day : "0" + day; //day 두자리로 저장
+    return year + "." + month + "." + day; //'-' 추가하여 yyyy-mm-dd 형태 생성 가능
+}
+// let userId = localStorage.getItem("id"); //id
+// let userClassfication = localStorage.getItem("classfication"); //student, teacher, manager
+// let userName = localStorage.getItem("name");
+let tempDate = new Date();
+let userDate = format(tempDate);
+let docData = {
+    // id: userId,
+    // classfication: userClassfication,
+    // title: userTitle,
+    // author: userName,
+    date: userDate,
+};
+db.collection("board")
+    .doc("recruitment")
+    .set(docData)
+    .then(() => {
+        console.log(`doc written`);
     });
